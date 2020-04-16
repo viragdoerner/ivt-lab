7638a7069699512b9573d4b5977f23357e75ef6e
viragdoerner <mail.dorner.virag@gmail.com>
2020-04-16 11:42:39 +0200

    modositas A

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -88,7 +88,7 @@ public class GT4500 implements SpaceShip {
           firingSuccess = secondaryTorpedoStore.fire(1);
           wasPrimaryFiredLast = false;
         }
-        
+        //modositas az A ágból
         
 
         break;
